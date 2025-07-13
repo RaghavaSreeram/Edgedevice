c813bd11243ef106228dd6529150275ac3b6645b
Raghava Sreeram <raghava10sreeram@gmail.com>
2025-07-13 13:53:58 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,1 +1,8 @@
-# Scan subnet for RTSP cameras
+import socket
+
+def scan_ports(ip, port=554, timeout=1):
+    try:
+        with socket.create_connection((ip, port), timeout=timeout):
+            return True
+    except:
+        return False
