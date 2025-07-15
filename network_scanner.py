6ffae14318fce77d937447e621e7cbaa969d5f0f
Raghava Sreeram <raghava10sreeram@gmail.com>
2025-07-15 14:03:21 +0530

    Delete network_scanner.py

--- network_scanner.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def scan_port(ip, port=554, timeout=2):
-    try:
-        with socket.create_connection((ip, port), timeout=timeout):
-            return True
-    except:
-        return False
